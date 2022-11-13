7d663519f22924b58dd7b4a0f4b58b46f7365859
YaMolod <skystep56@gmail.com>
2022-11-13 18:01:50 +0500

    Basic structure

---
+++ app/src/main/java/com/example/vpn/ZachetVpnService.java
@@ -0,0 +1,25 @@
+package com.example.vpn;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.VpnService;
+import android.os.Handler;
+import android.os.Message;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Toast;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ZachetVpnService extends VpnService
+{
+
+}
