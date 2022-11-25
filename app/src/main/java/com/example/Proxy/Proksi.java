0efc62825218de236e460e1a4239a7051e54936f
YaMolod <skystep56@gmail.com>
2022-11-25 21:40:34 +0500

    LittleProxy

--- app/src/main/java/com/example/Proxy/Proksi.java
+++ app/src/main/java/com/example/Proxy/Proksi.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import org.littleshoot.proxy.*;
+import org.littleshoot.proxy.impl.*;
+
 public class Proksi extends Activity{
 
     @Override
@@ -11,6 +14,11 @@ public class Proksi extends Activity{
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HttpProxyServer server =
+                DefaultHttpProxyServer.bootstrap()
+                        .withPort(8080)
+                        .start();
     }
 
     @Override
